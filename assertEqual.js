78bb0dac218530c5c8fc937d4b9aece196fa7f11
FatemehAmir <f.amirkhanloo64@gmai.com>
2021-01-07 00:13:32 +0000

    add file

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  
+    //console.assert(assertEqual(actual === expected),);
+    if(actual=== expected){
+        console.log('Assertion passed: %s === %s', actual , expected) 
+    }else{
+        console.log('Assertion failed: %s !==  %s ', actual , expected)
+    }
+        
+        
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+/*Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
+Assertion Passed: 1 === 1*/
