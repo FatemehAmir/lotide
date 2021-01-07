90ee42e5ca60260f9f90fa10fa706118545c344f
FatemehAmir <f.amirkhanloo64@gmai.com>
2021-01-07 01:31:43 +0000

    add new file

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,10 @@
 const assertEqual = function(actual, expected) {
-  
     //console.assert(assertEqual(actual === expected),);
     if(actual=== expected){
-        console.log('Assertion passed: %s === %s', actual , expected) 
-    }else{
-        console.log('Assertion failed: %s !==  %s ', actual , expected)
+        console.log('Assertion passed: %s === %s', actual, expected) 
+    } else{
+        console.log('Assertion failed: %s !==  %s ', actual, expected)
     }
-        
-        
 };
 
 // TEST CODE
