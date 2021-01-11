fd11a511543b25c07e8230d3c95f99deffcfd5ea
FatemehAmir <f.amirkhanloo64@gmai.com>
2021-01-11 22:50:15 +0000

    added require

--- assertEqual.js
+++ assertEqual.js
@@ -8,8 +8,23 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+
+module.exports = assertEqual;
+
+
+
 
 /*Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
 Assertion Passed: 1 === 1*/
+//FUNCTIONS THAT NEED TO BE TESTED
+//eqArrays([1, 2, 3], [1, 2, 3]) // => true
+//eqArrays([1, 2, 3], [3, 2, 1]) // => false
+//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
+//TESTS THAT WILL RUN (assert equals runs our tests)
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+//assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
