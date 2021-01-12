3fb173fbffa8c4ff5f9205e05e2058951a1258ea
FatemehAmir <f.amirkhanloo64@gmai.com>
2021-01-12 00:54:27 +0000

    added test

---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,10 @@
+//const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+//const assertArraysEqualTest = function(array1, array2) {
+//  
+//}
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+//assertArraysEqualTest();
+//module.exports=assertArraysEqualTest;
+
