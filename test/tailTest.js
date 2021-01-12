87f4044c7faec6eb6e708341c05643be385a303e
FatemehAmir <f.amirkhanloo64@gmai.com>
2021-01-12 04:34:45 +0000

    added

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,21 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const tail= require('../tail');
+// const tailTest=function(){
+//   assertArraysEqual(tail([1, 2, 3]), [2 ,3]);
+// }
+// tailTest();
+// module.exports = tailTest;
+const  assert= require("assert");
 const tail= require('../tail');
-const tailTest=function(){
-  assertArraysEqual(tail([1, 2, 3]), [2 ,3]);
-}
-tailTest();
-module.exports = tailTest;
+
+ describe("#tail", () => {
+   it("returns [ 1 ,2 , 3 ]for [5 ,1, 2, 3]", () => {
+     assert.deepEqual(tail([1, 2, 3]), [2 ,3 ]);
+   });
+   it("returns ['5'] for ['5']", () => {
+    assert.deepEqual(tail([5]), [5]); 
+  });
+  it("returns [5] for ['5']", () => {
+    assert.deepEqual(tail([5]), [5 , 2]); 
+  });
+ });
